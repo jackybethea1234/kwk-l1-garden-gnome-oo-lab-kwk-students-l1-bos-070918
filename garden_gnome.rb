8bd1fb54976fb5e930e0763a91a243db677b6719
jackybethea1234 <jackybethea@gmail.com>
2018-07-13 17:58:24 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,6 +1,10 @@
 # Code your instances here
 class GardenGnome
   attr_accessor :name, :age, :gluten_allergy
+  attr_reader :personality
+  
+  def initialize(personality)
+    @personality = "evil"
   
 end 
 
